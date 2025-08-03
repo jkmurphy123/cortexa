4b71378371152f1dc82208a32dcfdfc81e00ae4e
jkmurphy123 <jkmurphy@zbzoom.net>
2025-08-03 06:45:40 -0400

    Try self balloon

--- ui_renderer.py
+++ ui_renderer.py
@@ -156,10 +156,13 @@ class MainWindow(QMainWindow):
         spec = self.personality.get("speech_balloon", {})
         balloon_w = spec.get("width", 400)
         balloon_h = spec.get("height", 250)
-        self.balloon_widget = SpeechBalloonWidget(spec, parent=self)
+        #self.balloon_widget = SpeechBalloonWidget(spec, parent=self)
         #self.balloon_widget.setFixedSize(balloon_w + 16, balloon_h + 16)
-        self.balloon_widget.setFixedSize(QSize(balloon_w, balloon_h))
-        self.layout.addWidget(self.balloon_widget, stretch=1)
+        #self.layout.addWidget(self.balloon_widget, stretch=1)
+        self.balloon_widget.setParent(self)
+        self.balloon_widget.move(spec.get("x_pos", 100), spec.get("y_pos", 50))
+        self.balloon_widget.setFixedSize(balloon_w, balloon_h)
+        self.balloon_widget.show()
 
         # Typing indicator
         self.typing_label = QLabel("")
